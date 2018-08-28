5e28de14856545445fcb252b3321a6c9467bcd54
Justin <lesko.justin@gmail.com>
2018-08-28 11:36:50 -0400

    removed login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from '../environments/environment';
 import { FormsModule } from '@angular/forms';
-import { LoginAuth } from './home-login/home-login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginAuth
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase, 'dell-boston-summit'),
     AngularFireDatabaseModule,
+    AngularFireAuthModule,
     FormsModule
   ],
   providers: [],
